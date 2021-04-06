675eb0b6b5fb4f1546069c900a3b3ba19474974c
pikalov <Sinov00@list.ru>
2021-04-06 15:34:25 +0700

    Ответ на второй тест

--- zd1/Test.cs
+++ zd1/Test.cs
@@ -30,36 +30,5 @@ namespace zd1
             }
             return answer;
         }
-
-        /// <summary>
-        /// Эта функция является проверосной для Zd1_Ansver
-        /// </summary>
-        /// <param name="num"></param>
-        /// <returns></returns>
-        public static string Check(int num)
-        {
-            string ans = "There is no check with number " + num.ToString();
-            Dictionary<char, int> output;
-            switch (num)
-            {
-                case 1:
-                    ans = "input: t5qr33d56q3\noutput:\n";
-                    output = Zd1_Ansver("t5qr33d56q3");
-                    foreach (char item in output.Keys)
-                    {
-                        ans += item + ":" + output[item].ToString() + "\n";
-                    }
-                    break;
-                case 2:
-                    ans = "input: ;f2*14'2444\noutput:\n";
-                    output = Zd1_Ansver(";f2*14'2444");
-                    foreach (char item in output.Keys)
-                    {
-                        ans += item + ":" + output[item].ToString() + "\n";
-                    }
-                    break;
-            }
-            return ans;
-        }
     }
 }
