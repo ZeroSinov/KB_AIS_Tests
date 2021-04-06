aa54a052bb13ee0f8ee724bf1c50899ec0807f26
Пикалов Сергей Михайлович <pro17Pikalov@stud.KHPK.RU>
2021-04-06 14:33:37 +0700

    Добавьте файлы проекта.

---
+++ zd1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zd1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
